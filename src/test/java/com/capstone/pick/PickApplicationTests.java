efb8fcec7dba0dc7980cc976b0461e817cf0af2b
MoonStar <moonsung0331@gmail.com>
2023-02-15 19:24:32 +0900

    Spring Initializer 를 통한 초기 프로젝트 생성

---
+++ src/test/java/com/capstone/pick/PickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.pick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
