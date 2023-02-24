4907053f4adc5fe4c4c61c7227c5fa83f9c9e18c
jihoon <wlgnsdl1016@naver.com>
2023-02-24 19:29:35 +0900

    #5 - 뷰 엔드포인트 테스트

---
+++ src/test/java/com/capstone/pick/controller/LoginControllerTest.java
@@ -0,0 +1,41 @@
+package com.capstone.pick.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("로그인 컨트롤러")
+@WebMvcTest(LoginController.class)
+@WithMockUser
+class LoginControllerTest {
+
+    private final MockMvc mvc;
+
+    public LoginControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[Login][GET][/] 메인 페이지")
+    @Test
+    public void 로그인_뷰_엔드포인트_테스트() throws Exception {
+
+        // When & Then
+        mvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk()) // 상태코드가 200인가?
+                .andExpect(result -> content().contentType(MediaType.TEXT_HTML)) // html 파일을 리턴해주는가?
+                .andExpect(view().name("/page/login")); // 리턴하는 뷰 이름은 무엇인가?
+                //.andExpect(model().attributeExists("")); // 뷰에 애트리뷰트가 존재하는가?
+    }
+
+
+}
