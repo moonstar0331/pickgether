3149ad9638778c98d0d69fd9fea0a7f083bd2d9d
JinJuJeong <wjdwlswn731@gmail.com>
2023-03-04 03:27:32 +0900

    #14 - 투표 DTO 작성
    투표 도메인을 기반으로 투표 DTO를 작성함

--- src/main/java/com/capstone/pick/dto/VoteDto.java
+++ src/main/java/com/capstone/pick/dto/VoteDto.java
@@ -1,4 +1,37 @@
 package com.capstone.pick.dto;
 
+import com.capstone.pick.domain.User;
+import com.capstone.pick.domain.Vote;
+import com.capstone.pick.domain.constant.Category;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
 public class VoteDto {
+    private Long id;
+    private UserDto userDto;
+    private String title;
+    private String content;
+    private Category category;
+    private LocalDateTime expiredAt;
+    private LocalDateTime createAt;
+    private LocalDateTime modifiedAt;
+    private boolean isMultiPick;
+
+    public Vote toEntity(User user) {
+        return Vote.builder()
+                .id(id)
+                .user(user)
+                .title(title)
+                .content(content)
+                .category(category)
+                .expiredAt(expiredAt)
+                .modifiedAt(modifiedAt)
+                .isMultiPick(isMultiPick).build();
+    }
 }
