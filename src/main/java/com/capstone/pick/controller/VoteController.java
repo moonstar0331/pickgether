ea8a9fdb9173c68811a2b22f7eb1bcae7e231c2a
JinJuJeong <wjdwlswn731@gmail.com>
2023-03-04 03:54:35 +0900

    #14 - 투표 게시글 저장 구현을 위한 최소 기능 구현

--- src/main/java/com/capstone/pick/controller/VoteController.java
+++ src/main/java/com/capstone/pick/controller/VoteController.java
@@ -1,9 +1,17 @@
 package com.capstone.pick.controller;
 
+import com.capstone.pick.dto.VoteDto;
+import com.capstone.pick.service.VoteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VoteController {
-
+    private static VoteService voteService;
+    @GetMapping("/")
+    public String postNewVote(VoteDto voteDto) {
+        // 엔티티로 받아와야하는데 일단 DTO로 받아옴
+        voteService.saveVote(voteDto);
+        return "";
+    }
 }
--- src/main/java/com/capstone/pick/service/VoteService.java
+++ src/main/java/com/capstone/pick/service/VoteService.java
@@ -1,4 +1,21 @@
 package com.capstone.pick.service;
 
+import com.capstone.pick.domain.User;
+import com.capstone.pick.dto.VoteDto;
+import com.capstone.pick.repository.UserRepository;
+import com.capstone.pick.repository.VoteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class VoteService {
+
+    private final UserRepository userRepository;
+    private final VoteRepository voteRepository;
+
+    public void saveVote(VoteDto dto) {
+        User user = userRepository.getReferenceById(dto.getUserDto().getUserId());
+        voteRepository.save(dto.toEntity(user));
+    }
 }
