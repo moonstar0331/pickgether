930800b0ca799cc48fb0892a7789e7c5c292f44b
MoonStar <moonsung0331@gmail.com>
2023-03-05 20:19:02 +0900

    #15 - 댓글 컨트롤러 TODO 작성 및 댓글 컨트롤러 엔드포인트 테스트 작성
    현재 게시글 기능 구현보다 투표 게시글 댓글 뷰가 먼저 작성되었다.
    실제로는 각 댓글 상세페이지는 투표 게시글과 연관이 있으므로 각 게시글과 연결되어야 한다.
    이에 대한 TODO 를 컨트롤러에 작성해두었다.
    엔드포인트 또한 게시글 기능이 구현되면 변경해야한다.

--- src/main/java/com/capstone/pick/controller/VoteCommentsController.java
+++ src/main/java/com/capstone/pick/controller/VoteCommentsController.java
@@ -3,6 +3,13 @@ package com.capstone.pick.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+/**
+ * TODO : 댓글 상세 페이지는 각 투표 게시글에 연결되는 것이기 때문에
+ *         추후에 게시글 구현이 완료되면 각 게시글에 연결하도록 하며,
+ *         추가로 테스트 케이스도 수정한다. -> @PathVariable 로 연결;
+ */
+
 @Controller
 public class VoteCommentsController {
 
