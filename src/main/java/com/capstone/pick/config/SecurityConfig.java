2ba71a67d9e4297ab73a780d2f40367a1e72e09b
MoonStar <moonsung0331@gmail.com>
2023-03-07 15:59:35 +0900

    #14 - Spring Security Configuration 수정
    혹시를 대비하여 모든 요청에 대하여 허용하던 코드는 주석으로 그대로 남겨둔다.
    
    또한 머지 이후에 추가 작업을 위해서 추가로 작성한 코드 또한 주석으로 남겨두고 머지 이후에 추가 작업을 수행한다.

--- src/main/java/com/capstone/pick/config/SecurityConfig.java
+++ src/main/java/com/capstone/pick/config/SecurityConfig.java
@@ -1,27 +1,69 @@
 package com.capstone.pick.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+//    private final UserService userService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .anyRequest()
-                .permitAll()
-                .and()
+//        return http.csrf()
+//                .disable()
+//                .authorizeHttpRequests()
+//                .anyRequest()
+//                .permitAll()
+//                .and()
+//                .formLogin()
+//                .loginPage("/page/login.html")
+//                .loginProcessingUrl("/login")
+//                .defaultSuccessUrl("/page/timeLine.html", true)
+//                .failureUrl("/login.html?error=true")
+//                .and()
+//                .build();
+
+        return http.httpBasic().disable()
+                .csrf().and()
+                .rememberMe().and()
+                .authorizeRequests()
+                .anyRequest().permitAll().and()
                 .formLogin()
                 .loginPage("/page/login.html")
                 .loginProcessingUrl("/login")
                 .defaultSuccessUrl("/page/timeLine.html", true)
-                .failureUrl("/login.html?error=true")
+                .failureUrl("/login.html?error=true").permitAll().and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/page/login.html")
                 .and()
                 .build();
     }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        return username -> {
+//            User user = userService.findByUsername(username);
+//            if(user == null) throw new UsernameNotFoundException("유저를 찾을 수 없습니다.");
+//            return user;
+//        }
+//    }
 }
