d83dd83858f03bcd46519e397a65833bfffea963
MoonStar <moonsung0331@gmail.com>
2023-03-08 16:04:40 +0900

    #14 - 시큐리티를 위한 설정 작성 및 UserPrincipal 작성

--- src/main/java/com/capstone/pick/config/SecurityConfig.java
+++ src/main/java/com/capstone/pick/config/SecurityConfig.java
@@ -1,69 +1,63 @@
 package com.capstone.pick.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
-
-//    private final UserService userService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http.csrf()
-//                .disable()
-//                .authorizeHttpRequests()
-//                .anyRequest()
-//                .permitAll()
-//                .and()
-//                .formLogin()
-//                .loginPage("/page/login.html")
-//                .loginProcessingUrl("/login")
-//                .defaultSuccessUrl("/page/timeLine.html", true)
-//                .failureUrl("/login.html?error=true")
-//                .and()
-//                .build();
 
-        return http.httpBasic().disable()
-                .csrf().and()
-                .rememberMe().and()
-                .authorizeRequests()
-                .anyRequest().permitAll().and()
+        return http.authorizeRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .mvcMatchers(HttpMethod.GET, "/timeLine").permitAll()
+//                        .anyRequest().authenticated()
+                )
                 .formLogin()
-                .loginPage("/page/login.html")
-                .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/page/timeLine.html", true)
-                .failureUrl("/login.html?error=true").permitAll().and()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/timeLine")
+                .and()
                 .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/page/login.html")
+                .logoutSuccessUrl("/")
                 .and()
                 .build();
     }
 
+//    @Bean
+//    protected UserDetailsService userDetailsService(@Autowired UserRepository userRepository) {
+//        return username -> userRepository
+//                .findById(username)
+//                .map(UserDto::from)
+//                .map(VotePrincipal::from)
+//                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
+//    }
+
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    protected UserDetailsService userDetailsService() {
-//        return username -> {
-//            User user = userService.findByUsername(username);
-//            if(user == null) throw new UsernameNotFoundException("유저를 찾을 수 없습니다.");
-//            return user;
-//        }
-//    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
 }
