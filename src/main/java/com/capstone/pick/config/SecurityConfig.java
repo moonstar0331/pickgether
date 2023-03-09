327914d8dd54cbb8295f3f1fc819e95853d7a125
MoonStar <moonsung0331@gmail.com>
2023-03-09 11:32:34 +0900

    #14 - 스프링 시큐리티 Config 구현
    기존에 적용하였던 인메모리에 테스트 유저를 저장하여 인증하는 방식에서 DB에 있는 유저를 인증할 수 있도록 구현

--- src/main/java/com/capstone/pick/config/SecurityConfig.java
+++ src/main/java/com/capstone/pick/config/SecurityConfig.java
@@ -1,33 +1,41 @@
 package com.capstone.pick.config;
 
+import com.capstone.pick.dto.UserDto;
+import com.capstone.pick.repository.UserRepository;
+import com.capstone.pick.security.VotePrincipal;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserRepository userRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        return http.authorizeRequests(auth -> auth
+        return http.httpBasic().disable()
+                .csrf().and()
+                .rememberMe().and()
+                .authorizeRequests(auth -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .mvcMatchers(HttpMethod.GET, "/timeLine").permitAll()
-//                        .anyRequest().authenticated()
+                        .mvcMatchers(HttpMethod.GET, "/", "/timeLine").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .formLogin()
-                .loginPage("/login").permitAll()
+                .loginPage("/login")
                 .defaultSuccessUrl("/timeLine")
+                .permitAll()
                 .and()
                 .logout()
                 .logoutSuccessUrl("/")
@@ -35,29 +43,17 @@ public class SecurityConfig {
                 .build();
     }
 
-//    @Bean
-//    protected UserDetailsService userDetailsService(@Autowired UserRepository userRepository) {
-//        return username -> userRepository
-//                .findById(username)
-//                .map(UserDto::from)
-//                .map(VotePrincipal::from)
-//                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
-//    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository
+                .findById(username)
+                .map(UserDto::from)
+                .map(VotePrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//        return new BCryptPasswordEncoder();
     }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
-    }
-
 }
