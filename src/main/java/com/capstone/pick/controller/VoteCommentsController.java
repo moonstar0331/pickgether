ccfc23a7131bbaa1bce3ba36d62b345da629653e
jihoon <wlgnsdl1016@naver.com>
2023-03-11 07:14:24 +0900

    #18 - 주석 작성

--- src/main/java/com/capstone/pick/controller/VoteCommentsController.java
+++ src/main/java/com/capstone/pick/controller/VoteCommentsController.java
@@ -17,11 +17,25 @@ public class VoteCommentsController {
 
     private final VoteCommentService voteCommentService;
 
+
+    /**
+     * 댓글을 조회한다
+     * @param voteId 게시글 id
+     * @return 댓글 목록 뷰
+     */
     @GetMapping("/{voteId}/comments")
     public String readComments(@PathVariable Long voteId) {
         return "page/comments";
     }
 
+
+    /**
+     * 댓글을 작성한다
+     * @param votePrincipal 사용자
+     * @param voteId 게시글 id
+     * @param commentForm 작성내용
+     * @return redirection to GET
+     */
     @PostMapping("/{voteId}/comments")
     public String saveComment(@AuthenticationPrincipal VotePrincipal votePrincipal,
                               @PathVariable Long voteId, CommentForm commentForm) {
@@ -30,6 +44,15 @@ public class VoteCommentsController {
         return "redirect:/" + voteId + "/comments";
     }
 
+
+    /**
+     * 댓글을 수정한다
+     * @param votePrincipal 사용자
+     * @param voteId 게시글 id
+     * @param commentId 댓글 id
+     * @param commentForm 작성내용
+     * @return redirection to GET
+     */
     @PostMapping("/{voteId}/comments/{commentId}")
     public String updateComment(@AuthenticationPrincipal VotePrincipal votePrincipal,
                                 @PathVariable Long voteId, @PathVariable Long commentId,
@@ -39,6 +62,14 @@ public class VoteCommentsController {
         return "redirect:/" + voteId + "/comments";
     }
 
+
+    /**
+     * 댓글을 삭제한다
+     * @param votePrincipal 사용자
+     * @param voteId 게시글 id
+     * @param commentId 투표 댓글 id
+     * @return redirection to GET
+     */
     @DeleteMapping("/{voteId}/comments/{commentId}")
     public String deleteComment(@AuthenticationPrincipal VotePrincipal votePrincipal,
                                 @PathVariable Long voteId, @PathVariable Long commentId) throws Exception {
@@ -46,4 +77,18 @@ public class VoteCommentsController {
         voteCommentService.deleteComment(commentId, votePrincipal.toDto().getUserId());
         return "redirect:/" + voteId + "/comments";
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
