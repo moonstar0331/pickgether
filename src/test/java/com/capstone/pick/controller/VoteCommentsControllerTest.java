e8b928a76a4032877235a3b442513500d2396a1c
MoonStar <moonsung0331@gmail.com>
2023-03-11 01:05:46 +0900

    #18 - 투표 댓글 작성 컨트롤러 테스트 구현
    #9355d3 에서 작성한 것과 같이 아직 뷰와 URI에 대한 의논이 없었기 때문에 뷰와 URI 에 대한 테스트 로직은 주석으로 남겨두었다.
    추후에 회의 이후에 뷰와 URI 부분을 구현하고 주석을 해제하고 적절한 로직을 구현하도록 한다.

--- src/test/java/com/capstone/pick/controller/VoteCommentsControllerTest.java
+++ src/test/java/com/capstone/pick/controller/VoteCommentsControllerTest.java
@@ -1,16 +1,29 @@
 package com.capstone.pick.controller;
 
 import com.capstone.pick.config.TestSecurityConfig;
+import com.capstone.pick.controller.form.CommentForm;
+import com.capstone.pick.dto.CommentDto;
+import com.capstone.pick.service.VoteCommentService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.TestExecutionEvent;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 댓글")
@@ -20,6 +33,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class VoteCommentsControllerTest {
 
     private final MockMvc mvc;
+
+    @MockBean
+    private VoteCommentService voteCommentService;
 
     public VoteCommentsControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -32,5 +48,40 @@ class VoteCommentsControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("page/comments"));
+    }
+
+
+    @WithUserDetails(value = "user", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("[comment][POST][/comments/new")
+    @Test
+    void saveVoteComment() throws Exception {
+
+        // given
+        long voteId = 1L;
+        CommentForm commentForm = CommentForm.builder()
+                .voteId(voteId)
+                .content("new comment")
+                .createdAt(LocalDateTime.now())
+                .modifiedAt(LocalDateTime.now())
+                .build();
+
+        willDoNothing().given(voteCommentService).saveComment(any(CommentDto.class));
+
+        // when
+        mvc.perform(post("/comments/new")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .flashAttr("commentForm", commentForm)
+                        .with(csrf())
+                )
+                .andExpect(status().is3xxRedirection());
+//                .andExpect(view().name("page/voteDetail"))
+//                .andExpect(redirectedUrl("/timeLine/{voteId}"));
+        /**
+         * TODO: 테스트 구현 시에 아직은 댓글 출력 화면에 대한 의논을 나누지 않았기 때문에
+         *       view 와 관련된 테스트는 주석으로 남겨둔다.
+          */
+
+        // then
+        then(voteCommentService).should().saveComment(any(CommentDto.class));
     }
 }
