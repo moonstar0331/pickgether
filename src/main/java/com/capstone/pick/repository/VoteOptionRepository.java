8e2a7eec4e750ae934a5b29578cc623dd1336dc8
choheeyeon <202015006@kyonggi.ac.kr>
2023-03-11 01:20:43 +0900

    #14 - voteOption 불러오기

--- src/main/java/com/capstone/pick/repository/VoteOptionRepository.java
+++ src/main/java/com/capstone/pick/repository/VoteOptionRepository.java
@@ -3,5 +3,8 @@ package com.capstone.pick.repository;
 import com.capstone.pick.domain.VoteOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VoteOptionRepository extends JpaRepository<VoteOption, Long> {
+    List<VoteOption> findAllByVoteId(Long vote_id);
 }
