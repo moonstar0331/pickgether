51047659fba7bc16f6b8b8d0d4c00f2709b71ebc
MoonStar <moonsung0331@gmail.com>
2023-03-12 21:07:34 +0900

    #14 - 투표 게시글, 게시글 댓글 컨트롤러 테스트 케이스 추가
    기존 테스트 코드에는 인증된 사용자와 인증이 되지 않은 사용자에 대한 검증이 진행되지 않았다.
    
    이에 좀 더 명확한 테스트를 진행할 수 있도록 인증에 대한 테스트 케이스를 추가하였다.

--- src/test/java/com/capstone/pick/controller/VoteControllerTest.java
+++ src/test/java/com/capstone/pick/controller/VoteControllerTest.java
@@ -4,7 +4,9 @@ import com.capstone.pick.config.TestSecurityConfig;
 import com.capstone.pick.controller.form.VoteForm;
 import com.capstone.pick.controller.form.VoteOptionFormDto;
 import com.capstone.pick.domain.constant.Category;
+import com.capstone.pick.domain.constant.DisplayRange;
 import com.capstone.pick.dto.HashtagDto;
+import com.capstone.pick.dto.UserDto;
 import com.capstone.pick.dto.VoteDto;
 import com.capstone.pick.dto.VoteOptionDto;
 import com.capstone.pick.service.VoteService;
@@ -17,6 +19,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,6 +27,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -44,9 +48,27 @@ class VoteControllerTest {
         this.mvc = mvc;
     }
 
+    @WithUserDetails(value = "user", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("[view][GET] 홈페이지 - 정상 호출, 인증된 사용자")
+    @Test
+    void home() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/timeline"))
+                .andExpect(redirectedUrl("/timeline"));
+    }
+
+    @DisplayName("[view][GET] 홈페이지 - 인증이 없을 땐 로그인 페이지로 이동")
+    @Test
+    void noLoginUser_home() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("**/login"));
+    }
+
     // TODO : 타임라인 조회의 경우 추후에 정렬과 페이징이 적용되면 테스트를 수정 해야한다.
     @WithUserDetails(value = "user", setupBefore = TestExecutionEvent.TEST_EXECUTION)
-    @DisplayName("[view][GET] 투표 게시글 조회 - 정상 호출")
+    @DisplayName("[view][GET] 투표 게시글 조회 - 정상 호출, 인증된 사용자")
     @Test
     void timeLine() throws Exception {
         // given
@@ -59,6 +81,18 @@ class VoteControllerTest {
                 .andExpect(model().attributeExists("votes"));
 
         // then
+    }
+
+    @DisplayName("[view][GET] 투표 게시글 조회 - 인증이 없을 땐 로그인 페이지로 이동")
+    @Test
+    void noLoginUser_timeline() throws Exception {
+        // when
+        mvc.perform(get("/timeline"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("**/login"));
+
+        // when
+        then(voteService).shouldHaveNoInteractions();
     }
 
     @WithUserDetails(value = "user", setupBefore = TestExecutionEvent.TEST_EXECUTION)
@@ -89,10 +123,69 @@ class VoteControllerTest {
         then(voteService).should().saveVote(any(VoteDto.class), Mockito.<VoteOptionDto>anyList(), Mockito.<HashtagDto>anyList());
     }
 
+    @DisplayName("[view][GET] 투표 게시글 수정 페이지 - 인증이 없을 땐 로그인 페이지로 이동")
+    @Test
+    void noLoginUser_editVote() throws Exception {
+        // given
+        long voteId = 1L;
+
+        // when & then
+        mvc.perform(get("/" + voteId + "/edit"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("**/login"));
+        then(voteService).shouldHaveNoInteractions();
+    }
+
+    @WithMockUser
+    @DisplayName("[view][GET] 투표 게시글 수정 페이지 - 정상 호출, 인증된 사용자")
+    @Test
+    void editVote_GET() throws Exception {
+        // given
+        long voteId = 1L;
+
+        UserDto userDto = UserDto.builder()
+                .userId("user")
+                .userPassword("password")
+                .email("email@email.com")
+                .nickname("nick")
+                .memo("memo")
+                .birthday(LocalDateTime.now())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        VoteDto voteDto = VoteDto.builder()
+                .userDto(userDto)
+                .title("title")
+                .content("new content")
+                .voteOptions(List.of(VoteOptionFormDto.builder().content("new option1").imageLink("/link/image1.png").build(),
+                        VoteOptionFormDto.builder().content("new option2").imageLink("/link/image2.png").build()))
+                .category(Category.WORRY)
+                .expiredAt(LocalDateTime.now().plusDays(5))
+                .createAt(LocalDateTime.now())
+                .modifiedAt(LocalDateTime.now())
+                .isMultiPick(true)
+                .displayRange(DisplayRange.PUBLIC)
+                .build();
+
+        given(voteService.getVote(voteId)).willReturn(voteDto);
+
+        // when
+        mvc.perform(get("/" + voteId + "/edit"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("/page/editForm"))
+//                .andExpect(model().attribute("voteForm", voteForm))
+//                .andExpect(model().attribute("optionDtos", optionDtos))
+                .andExpect(model().attribute("voteDto", voteDto));
+
+        // then
+        then(voteService).should().getVote(voteId);
+    }
+
     @WithUserDetails(value = "user", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     @DisplayName("[view][POST] 투표 게시글 수정 - 정상 호출")
     @Test
-    void updateVote() throws Exception {
+    void editVote_POST() throws Exception {
         // given
         long voteId = 1L;
 
