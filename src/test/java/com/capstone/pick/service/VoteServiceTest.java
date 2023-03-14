226d4e40459474f80adcac0c1d198609dd56b736
MoonStar <moonsung0331@gmail.com>
2023-03-14 13:29:28 +0900

    #34 -  투표 게시글 수정 로직 테스트 작성
    투표 게시글 수정 기능의 서비스 로직에 대해서 정상 호출과 예외 처리에 대한 테스트 케이스를 작성하였다.

--- src/test/java/com/capstone/pick/service/VoteServiceTest.java
+++ src/test/java/com/capstone/pick/service/VoteServiceTest.java
@@ -1,7 +1,5 @@
 package com.capstone.pick.service;
 
-import com.capstone.pick.controller.form.VoteForm;
-import com.capstone.pick.controller.form.VoteOptionFormDto;
 import com.capstone.pick.domain.*;
 import com.capstone.pick.domain.constant.Category;
 import com.capstone.pick.domain.constant.DisplayRange;
@@ -18,10 +16,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -57,15 +55,11 @@ public class VoteServiceTest {
 
         User user = createUser();
 
-        Vote vote1 = createVote(user, "title1", "content1");
-        Vote vote2 = createVote(user, "title2", "content2");
+        Vote vote1 = createVote(1L, user, "title1", "content1");
+        Vote vote2 = createVote(2L, user, "title2", "content2");
 
         votes.add(vote1);
         votes.add(vote2);
-
-        // 단위 테스트를 위해서 vote 의 null 값에 채워주는 용도
-        ReflectionTestUtils.setField(vote1, "id", 1L);
-        ReflectionTestUtils.setField(vote2, "id", 2L);
 
         given(voteRepository.findAll()).willReturn(votes);
 
@@ -85,38 +79,12 @@ public class VoteServiceTest {
         assertThat(voteDtos.size()).isEqualTo(2);
     }
 
-    private static Vote createVote(User user, String title, String content) {
-        return Vote.builder()
-                .user(user)
-                .title(title)
-                .content(content)
-                .category(Category.FREE)
-                .createAt(LocalDateTime.now())
-                .expiredAt(LocalDateTime.now().plusDays(3))
-                .isMultiPick(true)
-                .displayRange(DisplayRange.PUBLIC)
-                .build();
-    }
-
-    private static User createUser() {
-        return User.builder()
-                .userId("user")
-                .userPassword("password")
-                .email("email@email.com")
-                .nickname("nick")
-                .memo("memo")
-                .birthday(LocalDateTime.now())
-                .createdAt(LocalDateTime.now())
-                .build();
-    }
-
     @DisplayName("투표 게시글 정보를 입력하면, 투표 게시글을 저장한다.")
     @Test
     void saveVote() {
         // given
         User user = createUser();
-        Vote vote = createVote(user, "new title", "new content #hi");
-        ReflectionTestUtils.setField(vote, "id", 1L);
+        Vote vote = createVote(1L, user, "new title", "new content #hi");
         VoteOption option1 = createVoteOption(vote, "option1", "/image/link1");
         VoteOption option2 = createVoteOption(vote, "option2", "/image/link2");
         List<VoteOptionDto> options = List.of(VoteOptionDto.from(option1), VoteOptionDto.from(option2));
@@ -146,15 +114,84 @@ public class VoteServiceTest {
         verify(voteHashtagRepository, atLeastOnce()).save(any(VoteHashtag.class));
     }
 
+    @DisplayName("투표 게시글의 수정 정보를 입력하면, 투표 게시글을 수정한다.")
+    @Test
+    void updateVote() {
+        // given
+        User user = createUser();
+        Vote vote = createVote(1L, user, "title", "content");
+        VoteDto voteDto = createVoteDto("new title", "new content");
+
+        VoteOption option1 = createVoteOption(vote, "option1", "/image/link1");
+        VoteOption option2 = createVoteOption(vote, "option2", "/image/link2");
+        ReflectionTestUtils.setField(option1, "id", 1L);
+        ReflectionTestUtils.setField(option2, "id", 2L);
+        List<VoteOptionDto> options = List.of(VoteOptionDto.from(option1), VoteOptionDto.from(option2));
+
+        Hashtag hashtag = createHashtag("hi");
+        List<HashtagDto> hashtagDtos = List.of(HashtagDto.from(hashtag));
+        ReflectionTestUtils.setField(hashtag, "id", 1L);
+
+        given(voteRepository.getReferenceById(voteDto.getId())).willReturn(vote);
+        given(userRepository.getReferenceById(voteDto.getUserDto().getUserId())).willReturn(user);
+
+        // when
+        voteService.updateVote(voteDto.getId(), voteDto, options, hashtagDtos);
+
+        // then
+        assertThat(vote)
+                .hasFieldOrPropertyWithValue("title", voteDto.getTitle())
+                .hasFieldOrPropertyWithValue("content", voteDto.getContent());
+        then(voteRepository).should().getReferenceById(voteDto.getId());
+        then(userRepository).should().getReferenceById(voteDto.getUserDto().getUserId());
+    }
+
+    @DisplayName("존재하지 않는 투표 게시글에 수정 정보를 입력하면, 경고 로그를 출력하고 아무 것도 하지 않는다.")
+    @Test
+    void updateVote_Exception() {
+        // given
+        VoteDto voteDto = createVoteDto("new title", "new content");
+
+        User user = createUser();
+        VoteOption option1 = createVoteOption(voteDto.toEntity(user), "option1", "/image/link1");
+        VoteOption option2 = createVoteOption(voteDto.toEntity(user), "option2", "/image/link2");
+        ReflectionTestUtils.setField(option1, "id", 1L);
+        ReflectionTestUtils.setField(option2, "id", 2L);
+        List<VoteOptionDto> options = List.of(VoteOptionDto.from(option1), VoteOptionDto.from(option2));
+
+        Hashtag hashtag = createHashtag("hi");
+        List<HashtagDto> hashtagDtos = List.of(HashtagDto.from(hashtag));
+        ReflectionTestUtils.setField(hashtag, "id", 1L);
+
+        given(voteRepository.getReferenceById(voteDto.getId())).willThrow(EntityNotFoundException.class);
+
+        // when
+        voteService.updateVote(voteDto.getId(), voteDto, options, hashtagDtos);
+
+        // then
+        then(voteRepository).should().getReferenceById(voteDto.getId());
+    }
+
+    private static VoteDto createVoteDto(String title, String content) {
+        return VoteDto.builder()
+                .id(1L)
+                .userDto(createUserDto())
+                .title(title)
+                .content(content)
+                .category(Category.FREE)
+                .expiredAt(LocalDateTime.now().plusDays(5))
+                .createAt(LocalDateTime.now())
+                .modifiedAt(LocalDateTime.now())
+                .isMultiPick(true)
+                .displayRange(DisplayRange.PUBLIC)
+                .build();
+    }
+
     private static VoteHashtag createVoteHashtag(Vote vote, Hashtag hashtag) {
         return VoteHashtag.builder()
                 .vote(vote)
                 .hashtag(hashtag)
                 .build();
-    }
-
-    private static Hashtag createHashtag(String content) {
-        return Hashtag.builder().content(content).build();
     }
 
     private static VoteOption createVoteOption(Vote vote, String content, String imageLink) {
@@ -165,10 +202,34 @@ public class VoteServiceTest {
                 .build();
     }
 
-    private static VoteOptionFormDto createOptionFormDto(String content, String imageLink) {
-        return VoteOptionFormDto.builder()
+    private static Hashtag createHashtag(String content) {
+        return Hashtag.builder().content(content).build();
+    }
+
+    private static Vote createVote(Long id, User user, String title, String content) {
+        Vote vote = Vote.builder()
+                .user(user)
+                .title(title)
                 .content(content)
-                .imageLink(imageLink)
+                .category(Category.FREE)
+                .createAt(LocalDateTime.now())
+                .expiredAt(LocalDateTime.now().plusDays(3))
+                .isMultiPick(true)
+                .displayRange(DisplayRange.PUBLIC)
+                .build();
+        ReflectionTestUtils.setField(vote, "id", id);
+        return vote;
+    }
+
+    private static User createUser() {
+        return User.builder()
+                .userId("user")
+                .userPassword("password")
+                .email("email@email.com")
+                .nickname("nick")
+                .memo("memo")
+                .birthday(LocalDateTime.now())
+                .createdAt(LocalDateTime.now())
                 .build();
     }
 
