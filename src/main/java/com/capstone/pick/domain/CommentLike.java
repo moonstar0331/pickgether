8bc4eb2426e82aaecf70341b93c7fdc55baeb782
choheeyeon <202015006@kyonggi.ac.kr>
2023-03-18 00:56:14 +0900

    #40 - 댓글 좋아요 기능 domain, dto 추가
    comment view에 사용하기 위한 CommentPostDto를 추가하였다.

---
+++ src/main/java/com/capstone/pick/domain/CommentLike.java
@@ -0,0 +1,27 @@
+package com.capstone.pick.domain;
+
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class CommentLike {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_like_id")
+    private Long id; // 투표 댓글 좋아요 id
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "vote_comment_id")
+    private VoteComment voteComment; // 투표 댓글 id
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    private User user; // 유저 id
+}
