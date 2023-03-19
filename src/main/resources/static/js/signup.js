37261cc83103eec3ff448ffc5b5b9f8116bd163f
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-03-19 09:23:16 +0900

    #37 - 비밀번호 확인 동일 여부 확인 추가

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -21,3 +21,23 @@ function signupValidation() {
         alert("빈칸이 있습니다.");
     }
 }
+
+function pw_pwcheck_isSame() {
+    var pw = $("#password").val();
+    var pw_check = $("#password_check").val();
+
+    var empty_message = ""
+    var success_message = "비밀번호가 일치합니다."
+    var fail_message = "동일한 비밀번호를 입력해주세요.";
+
+    if (pw_check == "") {
+        document.getElementById("pw_check_img").src = "";
+        document.getElementById("pwCheckLabel").innerHTML = empty_message;
+    } else if (pw == pw_check) {   // 비밀번호 확인 성공
+        document.getElementById("pw_check_img").src = "/images/icons/check.svg";
+        document.getElementById("pwCheckLabel").innerHTML = success_message;
+    } else {    // 비밀번호 확인 실패
+        document.getElementById("pw_check_img").src = "/images/icons/cross-small.svg";
+        document.getElementById("pwCheckLabel").innerHTML = fail_message;
+    }
+}
