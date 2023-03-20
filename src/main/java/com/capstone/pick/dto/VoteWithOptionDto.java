5751831ada9fdf5c30f5bcceb82ac1ca288763f4
MoonStar <moonsung0331@gmail.com>
2023-03-20 16:32:16 +0900

    리팩토링 : 게시글과 선택지 양방향 연관관계에 따른 DTO 작성
    게시글과 선택지를 같이 반환할 수 있는 DTO 작성

---
+++ src/main/java/com/capstone/pick/dto/VoteWithOptionDto.java
@@ -0,0 +1,48 @@
+package com.capstone.pick.dto;
+
+import com.capstone.pick.domain.Vote;
+import com.capstone.pick.domain.constant.Category;
+import com.capstone.pick.domain.constant.DisplayRange;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VoteWithOptionDto {
+
+    private Long id;
+    private UserDto userDto;
+    private String title;
+    private String content;
+    private Category category;
+    private LocalDateTime expiredAt;
+    private LocalDateTime createAt;
+    private LocalDateTime modifiedAt;
+    private boolean isMultiPick;
+    private DisplayRange displayRange;
+    private List<VoteOptionDto> voteOptionDtos;
+
+    public static VoteWithOptionDto from(Vote entity) {
+        return VoteWithOptionDto.builder()
+                .id(entity.getId())
+                .userDto(UserDto.from(entity.getUser()))
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .category(entity.getCategory())
+                .expiredAt(entity.getExpiredAt())
+                .createAt(entity.getCreateAt())
+                .modifiedAt(entity.getModifiedAt())
+                .isMultiPick(entity.isMultiPick())
+                .voteOptionDtos(entity.getVoteOptions().stream()
+                        .map(VoteOptionDto::from).collect(Collectors.toList()))
+                .build();
+    }
+}
