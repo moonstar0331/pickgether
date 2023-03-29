9a3a02594a4ce97e904055d6f1e6e326bf6a469e
jihoon <wlgnsdl1016@naver.com>
2023-03-29 14:14:45 +0900

    #57 - 프로필 뷰 구현

---
+++ src/main/java/com/capstone/pick/controller/ProfilesController.java
@@ -0,0 +1,15 @@
+package com.capstone.pick.controller;
+
+import com.capstone.pick.security.VotePrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfilesController {
+
+    @GetMapping("/profile")
+    public String profiles(@AuthenticationPrincipal VotePrincipal votePrincipal) {
+        return "page/profile";
+    }
+}
---
+++ src/test/java/com/capstone/pick/controller/ProfilesControllerTest.java
@@ -0,0 +1,46 @@
+package com.capstone.pick.controller;
+
+import com.capstone.pick.config.TestSecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("프로필 컨트롤러")
+@Import(TestSecurityConfig.class)
+@WebMvcTest(ProfilesController.class)
+@ComponentScan(basePackages = "com.capstone.pick.config")
+@MockBean(JpaMetamodelMappingContext.class)
+class ProfilesControllerTest {
+
+    private final MockMvc mvc;
+
+    public ProfilesControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[GET][/profile] 프로필 페이지")
+    @WithUserDetails(value = "user", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @Test
+    public void 프로필_뷰_엔드포인트_테스트() throws Exception {
+
+        mvc.perform(get("/profile"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("page/profile"));
+    }
+
+}
