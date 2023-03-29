95206d8367538dec3e4f40e6013204d33fc926d6
MoonStar <moonsung0331@gmail.com>
2023-03-29 15:38:30 +0900

    #54 - 투표 참여 기능 테스트 케이스 작성

--- src/main/java/com/capstone/pick/controller/request/PickRequest.java
+++ src/main/java/com/capstone/pick/controller/request/PickRequest.java
@@ -1,10 +1,12 @@
 package com.capstone.pick.controller.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PickRequest {
