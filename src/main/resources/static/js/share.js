310303a2f92fe96fcbb8ce4d5f62bd5fa43c980d
choheeyeon <202015006@kyonggi.ac.kr>
2023-04-12 16:09:15 +0900

    #77 - 카카오톡 공유 링크 설정
    기존에는 현재 url을 공유하도록 되어 있어 http://localhost:8080으로 공유되었다. 도메인을 반영하여 공유 링크를 수정하였다.

--- src/main/resources/static/js/share.js
+++ src/main/resources/static/js/share.js
@@ -1,33 +1,33 @@
-$(document).ready(function () {
+function sendLink(id) {
+    let url = 'https://pickgether.shop/' + id + '/detail';
     if (!Kakao.isInitialized()) {
         Kakao.init('31a176393c0166bd16a25e9e62aa2939');
     }
-    Kakao.Share.createDefaultButton({
-        container: '#kakaotalk-sharing-btn',
+    Kakao.Link.sendDefault({
         objectType: 'feed',
         content: {
-            title: document.getElementById('vote-title').innerText,
-            description: document.getElementById('vote-content').innerText,
+            title: document.getElementById('vote-title-' + id).innerText,
+            description: document.getElementById('vote-content-' + id).innerText,
             imageUrl:
                 'https://user-images.githubusercontent.com/90389517/229438674-6c51ab1c-9508-4722-a041-19179d6ba06f.png',
             link: {
                 // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
-                mobileWebUrl: document.URL,
-                webUrl: document.URL,
+                mobileWebUrl: url,
+                webUrl: url,
             },
         },
         social: {
-            likeCount: Number(document.getElementById('vote-participantNum').innerText),
-            commentCount: Number(document.getElementById('vote-commentNum').innerText),
+            likeCount: Number(document.getElementById('vote-participantNum-' + id).innerText),
+            commentCount: Number(document.getElementById('vote-commentNum-' + id).innerText),
         },
         buttons: [
             {
                 title: 'Pick Together',
                 link: {
-                    mobileWebUrl: document.URL,
-                    webUrl: document.URL,
+                    mobileWebUrl: url,
+                    webUrl: url,
                 },
             },
         ],
-    });
-});
+    })
+}
