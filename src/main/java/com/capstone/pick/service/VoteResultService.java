8e796645f439d4d7cc8b8cf7c1e747ed5dfa8276
choheeyeon <202015006@kyonggi.ac.kr>
2023-04-24 18:15:38 +0900

    #101 - 성별, 나이, 주소, 직업별 하둡 적용
    성능 개선 필요

--- src/main/java/com/capstone/pick/service/VoteResultService.java
+++ src/main/java/com/capstone/pick/service/VoteResultService.java
@@ -35,11 +35,13 @@ public class VoteResultService {
     public List<String[]> getVoteResults(Long voteId) throws Exception {
         List<VoteOption> voteOptions = voteOptionRepository.findAllByVoteId(voteId);
         String filename = voteId + "_" + voteOptions.get(0).getVote().getTitle();
+        List<String[]> result = new ArrayList<>();
+        result.add(new String[]{"vote_opion_id", "vote_option_content"});
 
-        // 투표 참여 csv 생성
-        String inputPath = "/tmp/" + filename + ".csv";
-        Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(inputPath), "UTF-8"));
+        Configuration conf = new Configuration();
         for (VoteOption vo : voteOptions) {
+            String inputPath = "/tmp/" + filename + ".csv";
+            Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(inputPath), "UTF-8"));
             for (Pick p : pickRepository.findAllByVoteOptionId(vo.getId())) {
                 writer.write(vo.getId() + "," +
                         vo.getContent() + "," +
@@ -49,11 +51,28 @@ public class VoteResultService {
                         p.getUser().getJob() + "\n"
                 );
             }
-        }
-        writer.close();
+            writer.close();
 
+            result.addAll(runMapreduce(conf, inputPath, vo.getId(), vo.getContent(), "gender", TokenizerMapper_gender.class));
+            result.addAll(runMapreduce(conf, inputPath, vo.getId(), vo.getContent(), "age_range", TokenizerMapper_ageRange.class));
+            result.addAll(runMapreduce(conf, inputPath, vo.getId(), vo.getContent(), "address", TokenizerMapper_address.class));
+            result.addAll(runMapreduce(conf, inputPath, vo.getId(), vo.getContent(), "job", TokenizerMapper_job.class));
+
+            // 임시 파일 삭제
+            FileSystem fs = FileSystem.get(conf);
+            fs.delete(new Path(inputPath), true);
+        }
+
+        String csvOutputPath = "/tmp/" + filename + "_analysis.csv";
+        CSVWriter csvWriter = new CSVWriter(new OutputStreamWriter(new FileOutputStream(csvOutputPath), "euc-kr"));
+        csvWriter.writeAll(result);
+        csvWriter.close();
+
+        return result;
+    }
+
+    public List<String[]> runMapreduce(Configuration conf, String inputPath, Long optionId , String optionContent, String column, Class mapper) throws Exception {
         // 하둡 설정을 로드
-        Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "vote analysis");
 
         // 입력 데이터를 분할하여 맵 태스크에서 병렬 처리할 수 있도록 함
@@ -61,7 +80,7 @@ public class VoteResultService {
         job.setInputFormatClass(TextInputFormat.class);
 
         // MapReduce 작업을 위한 설정
-        job.setMapperClass(TokenizerMapper_gender.class);
+        job.setMapperClass(mapper);
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(IntWritable.class);
 
@@ -80,26 +99,19 @@ public class VoteResultService {
 
         // 맵리듀스 결과를 CSV 파일로 저장
         String outputPathStr = "/tmp/result/part-r-00000";
+
         List<String[]> result = new ArrayList<>();
-        result.add(new String[]{"vote_opion_id", "vote_option_content", "gender"});
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(outputPathStr), "euc-kr"));
         String line;
         while ((line = br.readLine()) != null) {
             String[] split = line.split("\t");
-            result.add(new String[]{"", "", split[0], split[1]});
+            result.add(new String[]{String.valueOf(optionId), optionContent, column, split[0], split[1]});
         }
         br.close();
-        // 현재는 선택지에 상관없이 모두 mapreduce 적용 중
 
         // 임시 파일 삭제
         FileSystem fs = FileSystem.get(conf);
-        fs.delete(new Path(inputPath), true);
-//        fs.delete(outputPath, true);
-
-        String csvOutputPath = "/tmp/" + filename + "_analysis.csv";
-        CSVWriter csvWriter = new CSVWriter(new OutputStreamWriter(new FileOutputStream(csvOutputPath), "euc-kr"));
-        csvWriter.writeAll(result);
-        csvWriter.close();
+        fs.delete(outputPath, true);
 
         return result;
     }
@@ -112,6 +124,45 @@ public class VoteResultService {
             VoteAnalysisParser parser = new VoteAnalysisParser(value);
             if (!parser.getGender().isEmpty()) {
                 word.set(parser.getGender());
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class TokenizerMapper_ageRange extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            VoteAnalysisParser parser = new VoteAnalysisParser(value);
+            if (!parser.getAge_range().isEmpty()) {
+                word.set(parser.getAge_range());
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class TokenizerMapper_address extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            VoteAnalysisParser parser = new VoteAnalysisParser(value);
+            if (!parser.getAddress().isEmpty()) {
+                word.set(parser.getAddress());
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class TokenizerMapper_job extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            VoteAnalysisParser parser = new VoteAnalysisParser(value);
+            if (!parser.getJob().isEmpty()) {
+                word.set(parser.getJob());
                 context.write(word, one);
             }
         }
