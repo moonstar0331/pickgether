0ffb9f5dc79908b1d1f99193581aa61c49ee09c4
choheeyeon <202015006@kyonggi.ac.kr>
2023-05-11 22:58:27 +0900

    #122 - 사진 업로드 경로 수정
    임시로 프로젝트 내부로 변경하였음

--- src/main/java/com/capstone/pick/service/FileUploadService.java
+++ src/main/java/com/capstone/pick/service/FileUploadService.java
@@ -5,24 +5,32 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 @RequiredArgsConstructor
 public class FileUploadService {
-    private String uploadPath = "/tmp";
+    public String saveFile(MultipartFile file) throws IOException {
+        if (!file.isEmpty()) {
+            String path = new File("").getAbsolutePath() + File.separator + File.separator + "upload" + File.separator;
+            File folder = new File(path);
 
-    public String saveFile(MultipartFile file) {
-        File saveFile = new File(uploadPath, file.getOriginalFilename());
-        try {
+            // 폴더가 존재하지 않을 경우
+            if (!folder.exists()) {
+                folder.mkdirs();
+            }
+
+            // 파일명 중복을 피하기 위해 이름 변경
+            String fileName = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")) + System.nanoTime() + file.getOriginalFilename();
+            String filePath = path + fileName;
+            File saveFile = new File(filePath);
+
             file.transferTo(saveFile);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (file.isEmpty())
+            return saveFile.getPath();
+        } else {
             return "";
-        else
-            return uploadPath + "/" + file.getOriginalFilename();
+        }
     }
 }
-
