9ab568decb3bb0c91ee6d26af5922e9ca45f8b09
MoonStar <moonsung0331@gmail.com>
2023-05-18 14:31:24 +0900

    #127 - 문자인증 기능 구현
    현재 문자인증을 위한 발신자 번호는 더미로 되어 있지만 테스트를 위해서는 본인의 핸드폰 번호를 입력하여야 한다.

--- src/main/java/com/capstone/pick/controller/SignupController.java
+++ src/main/java/com/capstone/pick/controller/SignupController.java
@@ -6,12 +6,19 @@ import com.capstone.pick.controller.request.SignupRequest;
 import com.capstone.pick.dto.UserDto;
 import com.capstone.pick.exeption.DuplicatedUserException;
 import lombok.RequiredArgsConstructor;
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.model.MessageType;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.Random;
 
 @RequiredArgsConstructor
 @Controller
@@ -19,6 +26,13 @@ public class SignupController {
 
     private final PasswordEncoder passwordEncoder;
     private final CustomUserDetailsService customUserDetailsService;
+
+    @Value("${spring.cool.api.key}")
+    private String api_key;
+
+    @Value("${spring.cool.api.secret}")
+    private String api_secret;
+
 
     /**
      * @param model 회원가입 폼 저장
@@ -53,5 +67,28 @@ public class SignupController {
                 .build();
         customUserDetailsService.save(userDto);
         return "redirect:/login";
+    }
+
+    @PostMapping("/sendSMS")
+    @ResponseBody
+    public String sendSMS(@RequestParam String toPhoneNumber) {
+        Random rand = new Random();
+        String numStr = "";
+
+        for(int i=0; i<4; i++) {
+            String ran = Integer.toString(rand.nextInt(10));
+            numStr += ran;
+        }
+
+        DefaultMessageService messageService = NurigoApp.INSTANCE.initialize(api_key, api_secret, "https://api.coolsms.co.kr");
+        Message message = new Message();
+        message.setFrom("01012345678"); // TODO: 문자인증을 진행하기 위해서는 자신의 핸드폰 번호를 입력하여 테스트 진행
+        message.setType(MessageType.SMS);
+        message.setTo(toPhoneNumber);
+        message.setText("인증번호는 [" + numStr + "] 입니다.");
+
+        messageService.sendOne(new SingleMessageSendingRequest(message));
+
+        return numStr;
     }
 }
