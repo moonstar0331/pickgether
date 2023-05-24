dcf86ef58c7af60e2b84b8d449dba39d2a25a54c
MoonStar <moonsung0331@gmail.com>
2023-05-25 00:57:18 +0900

    리팩토링 : 유저 캐시 리포지토리 수정 구현
    기존 redis 에 로그인한 사용자의 정보를 사용하는데에 있어서 구현이 오류가 있어 이를 수정하였음

--- src/main/java/com/capstone/pick/repository/cache/UserCacheRepository.java
+++ src/main/java/com/capstone/pick/repository/cache/UserCacheRepository.java
@@ -5,9 +5,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
 import java.time.Duration;
 import java.util.Optional;
 
@@ -16,20 +18,36 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserCacheRepository {
 
-    private final RedisTemplate<String, String> redisTemplate;
+    private final RedisTemplate<String, UserDto> redisTemplate;
+    private HashOperations<String, String, String> hashOperations;
     private final ObjectMapper objectMapper;
     private final static Duration CACHE_TTL = Duration.ofDays(3);
 
+    @PostConstruct
+    public void init() {
+        this.hashOperations = redisTemplate.opsForHash();
+    }
+
     public void setUser(UserDto user) {
         try {
-            redisTemplate.opsForValue().set(getKey(user.getUserId()), serializeUserDto(user), CACHE_TTL);
+            hashOperations.put("USER", user.getUserId(), serializeUserDto(user));
+//            redisTemplate.opsForValue().set(getKey(user.getUserId()), serializeUserDto(user), CACHE_TTL);
         } catch (Exception e) {
             log.error(e.getMessage());
         }
     }
 
+//    public Optional<UserDto> getUser(String username) throws JsonProcessingException {
+//        return Optional.ofNullable(deserializeUserDto(redisTemplate.opsForValue().get(getKey(username))));
+//    }
+
     public Optional<UserDto> getUser(String username) throws JsonProcessingException {
-        return Optional.ofNullable(deserializeUserDto(redisTemplate.opsForValue().get(getKey(username))));
+//        return Optional.ofNullable(deserializeUserDto(redisTemplate.opsForValue().get(getKey(username))));
+        return Optional.ofNullable(deserializeUserDto(hashOperations.get("USER", username)));
+    }
+
+    public void update(UserDto userDto) {
+        setUser(userDto);
     }
 
     private String getKey(String username) {
